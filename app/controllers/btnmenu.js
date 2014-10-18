f0d372408fc23118ffb1ea8c3f09368262022462
shivasolutions <shivasolutions@gmail.com>
2014-10-18 23:41:19 +0200

    | Añadidos nuevos controladores y vistas parciales.

--- app/controllers/btnmenu.js
+++ app/controllers/btnmenu.js
@@ -1,32 +1,38 @@
 var args = arguments[0] || {};
 
-function showMenu(e) {
-    // var menu = $.menu;
-    var menu = Alloy.createController('menu');
-    
-    
-    Ti.API.Info(menu.menu);
 
-    if (menu.toggle == true) {
-        //anima la ventana para que en 400ms se mueva 150 px a la izquierda con el EASE IN OUT
-        menu.animate({
-            right : '-70%',
-            duration : 400,
-            curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
-        });
-        hideDark(e);
-        menu.toggle = false;
-    }
-    //Si esta cerrado
-    else {
-        //anima la ventana para que en 400ms se mueva 150 px a la izquiera con el EASE IN OUT
-        menu.animate({
-            right : 0,
-            duration : 400,
-            curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
-        });
-        showDark(null);
-        menu.toggle = true;
-    }
 
-}
+$.menuButton.addEventListener('click', function(e) { 
+    $.trigger('click', e); 
+}); 
+
+// function showMenu(e) {
+    // // var menu = $.menu;
+    // var menu = Alloy.createController('menu');
+//     
+//     
+    // Ti.API.Info(menu.menu);
+// 
+    // if (menu.toggle == true) {
+        // //anima la ventana para que en 400ms se mueva 150 px a la izquierda con el EASE IN OUT
+        // menu.animate({
+            // right : '-70%',
+            // duration : 400,
+            // curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+        // });
+        // hideDark(e);
+        // menu.toggle = false;
+    // }
+    // //Si esta cerrado
+    // else {
+        // //anima la ventana para que en 400ms se mueva 150 px a la izquiera con el EASE IN OUT
+        // menu.animate({
+            // right : 0,
+            // duration : 400,
+            // curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+        // });
+        // showDark(null);
+        // menu.toggle = true;
+    // }
+// 
+// }
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,8 +3,9 @@ function doClick(e) {
 }
 
 function showMenu(e) {
-    var menu = $.menu;
-
+    // var menu = $.menu;
+    var menu = $.menu.getView('menu');
+    Ti.API.info(menu.toggle);
     if (menu.toggle == true) {
         //anima la ventana para que en 400ms se mueva 150 px a la izquierda con el EASE IN OUT
         menu.animate({
@@ -30,51 +31,56 @@ function showMenu(e) {
 }
 
 function swipeMenu(e) {
-    var menu = $.menu;
+    var menu = $.menu.getView('menu');
+    ;
 
     Ti.API.info(e.direction);
     if (menu.toggle == true && e.direction == 'right') {
-            menu.animate({
-                right : '-70%',
-                duration : 400,
-                curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
-            });
-            hideDark(e);
-            menu.toggle = false;
+        menu.animate({
+            right : '-70%',
+            duration : 400,
+            curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+        });
+
+        hideDark(e);
+        menu.toggle = false;
     }
 
     if (menu.toggle == false && e.direction == 'left') {
-            menu.animate({
-                right : 0,
-                duration : 400,
-                curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
-            });
-            
-            showDark(null);
-            menu.toggle = true;
+        menu.animate({
+            right : 0,
+            duration : 400,
+            curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+        });
+
+        showDark(null);
+        menu.toggle = true;
     }
 }
 
 function showDark(e) {
-    var dark = $.dark;
+    var dark = $.dark.getView('dark');
     dark.visible = true;
-    
+
     dark.animate({
-       opacity: 0.5,
-       duration: 400,
-       curve: Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+        opacity : 0.5,
+        duration : 400,
+        curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
     });
-    
+
 }
 
 function hideDark(e) {
-    var dark = $.dark;
-    
+    var dark = $.dark.getView('dark');
+
     dark.animate({
-       opacity: 0,
-       duration: 400,
-       curve: Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+        opacity : 0,
+        duration : 400,
+        curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
     });
 }
+
+$.index.navBarHidden = true;
+$.index.orientationModes = [Ti.UI.PORTRAIT];
 
 $.index.open();
