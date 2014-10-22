b01797af51fc8d3c2d7b256a96c4484ea0c44884
Gabriel <gaby@ampercode.com>
2014-10-22 20:38:58 +0200

    | Cambiada configuración para ocultar TitleBar. | Pruebas con 2dMatrix para las animaciones.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -8,13 +8,24 @@ function showMenu(e) {
     Ti.API.info(menu.toggle);
     if (menu.toggle == true) {
         //anima la ventana para que en 400ms se mueva 150 px a la izquierda con el EASE IN OUT
-        menu.animate({
-            right : '-70%',
-            duration : 400,
-            curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
-        });
+        // menu.animate({
+            // right : '-70%',
+            // duration : 400,
+            // curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+        // });
+        var matrix2d = Ti.UI.create2DMatrix();
+		matrix2d = matrix2d.rotate(20); // in degrees
+		matrix2d = matrix2d.scale(1.5); // scale to 1.5 times original size
+		var a = Ti.UI.createAnimation({
+			transform: matrix2d,
+			duration: 300,
+			autoreverse: true,
+			repeat: 2
+		});
+		menu.animate(a);
         hideDark(e);
         menu.toggle = false;
+        hideDark(e);
     }
     //Si esta cerrado
     else {
@@ -60,12 +71,13 @@ function swipeMenu(e) {
 
 function showDark(e) {
     var dark = $.dark.getView('dark');
+    Ti.API.info(dark.visible);
     dark.visible = true;
 
     dark.animate({
+    	curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT,
         opacity : 0.5,
-        duration : 400,
-        curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+        duration : 50
     });
 
 }
@@ -74,13 +86,15 @@ function hideDark(e) {
     var dark = $.dark.getView('dark');
 
     dark.animate({
+    	curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT,
         opacity : 0,
-        duration : 400,
-        curve : Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+        duration : 50
     });
 }
 
-$.index.navBarHidden = true;
+// $.index.navBarHidden = false;
 $.index.orientationModes = [Ti.UI.PORTRAIT];
+Ti.API.info($.index.navBarHidden);
+
 
 $.index.open();
